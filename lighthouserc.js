0bc417116b5184a08ad882a0c8efaf6cac188dc7
Venkata Kommareddy <venkata.kommareddy@gmail.com>
2021-07-18 22:07:23 -0400

    added https

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
 	collect: {
-      url: ['http://localhost:4200/'],
+      url: ['https://localhost:4200/'],
     },
     upload: {
       target: 'temporary-public-storage',
