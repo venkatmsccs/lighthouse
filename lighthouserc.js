61a92b27dade403dfbe7eba332e857477dcd4d9a
kkknet <kottekiran@gmail.com>
2021-07-20 22:07:29 -0400

    testing port change and url change

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
 	collect: {
-      url: ['http://localhost:9000/lighthouse-ci/index.html'],
+      url: ['http://localhost:4200/index.html'],
     },
     upload: {
       target: 'temporary-public-storage',
